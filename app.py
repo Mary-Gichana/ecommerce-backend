2e3373affa5383bd52ff2f43e3b662710c82b6f9
Mary Monchari Gichana <marygichana95@gmail.com>
2025-05-20 16:24:33 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask
+from flask import Flask, jsonify
+from models import db, Product, User
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
 
 @app.route('/')
 def home():
     return '<h1>Hello World!</h1>'
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{
+        'id': user.id,
+        'name': user.name,
+        'email': user.email
+    } for user in users])
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    return jsonify([{
+        'id': product.id,
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'category': product.category,
+        'stock': product.stock,
+        'image': product.image,
+        'user_id': product.user_id
+    } for product in products])
+
+@app.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get_or_404(id)
+    return jsonify({
+        'id': product.id,
+        'name': product.name,
+        'description': product.description,
+        'price': product.price,
+        'category': product.category,
+        'stock': product.stock,
+        'image': product.image,
+        'user_id': product.user_id
+    })
 
 if __name__ == '__main__':
     app.run(port=5555,debug=True)
